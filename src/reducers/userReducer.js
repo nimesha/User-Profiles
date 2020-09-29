abb034dc129c077cd98d35fd5ca7a10271b0e1ef
Nimesha Gunawardana <nimeshae@gmail.com>
2020-09-29 10:39:58 +0530

    user reducer with add, update, remove (without function)

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+export const userReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_USER':
+            return state;
+        case 'UPDATE_USER':
+            return state;
+        case 'REMOVE_USER':
+            return state;
+        default:
+            return state;
+    }
+} 
