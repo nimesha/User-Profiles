a66a31eea55e7434bb8476de697d1635420d81b8
Nimesha Gunawardana <nimeshae@gmail.com>
2020-09-30 16:12:34 +0530

    validation changes

--- src/helpers/validation/user.js
+++ src/helpers/validation/user.js
@@ -1,20 +1,29 @@
-
 import * as yup from "yup";
 
-
 const schema = yup.object().shape({
-    firstName: yup.string().max(60, "Too long").required("First Name Required"),
-    lastName: yup.string().max(5, "Too long"),
+    firstName: yup.string().max(60, "First name max lenght exceed").required("First Name Required"),
+    lastName: yup.string().max(60, "Last name max lenght exceed"),
+    contact: yup.string().max(12, "Contactg max lenght exceed"),
+    address: yup.string().max(255, "Address max lenght exceed"),
     email: yup.string().email("Enter Valid Email").max(255).required("Email Address Required"),
-    profilePic: yup.mixed()
-      .required("You need to provide a file")
-      .test("fileSize", "The file is too large", (value) => {
-        return value && value[0].size <= 3000000;
-      })
-      .test("type", "We only support JPEG/PNG", (value) => {
-        return value && (value[0].type === "image/jpeg" || value[0].type === "image/png" ) ;
-      }),
-  });
+    dob: yup.date().min(new Date('01-01-1900'))
+    .max(new Date(new Date().setDate(new Date()
+    .getDate() + 1)), 'Not allow to add future date')
+    .required("Date of Birth Required"),
+    // profilePic: yup.mixed()
+    //   .test("fileSize", "The file is too large", (value) => {
+    //       console.log(value && value[0].size <= 3000000)
+    //     return value && value[0].size <= 3000000;
+    //   })
+    //   .test("type", "We only support JPEG/PNG", (value) => {
+    //     return value && (value[0].type === "image/jpeg" || value[0].type === "image/png" ) ;
+
+    //   })
+    //   .test("", "Profile image Required", (value) => {
+    //     return false;
+    // })
+});
+
 
 
 export default schema;
